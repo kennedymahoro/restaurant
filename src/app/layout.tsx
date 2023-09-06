0e58341f5097a4548d24a0aaec4f4be74fcf926e
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2023-09-05 23:40:00 -0400

    removed more junk and added matadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,8 +16,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className='w-full h-full'>
+      <body >{children}</body>
     </html>
   )
 }

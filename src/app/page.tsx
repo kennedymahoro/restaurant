b39d532a45f5e1741347c78148ca7c121d2cca6b
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2023-09-05 23:40:41 -0400

    worked on adding props and adding the components to the main function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 //import Image from 'next/image'
+import Navbar from "@/components/nav"
+//import Footer from "@/components/footer"
 
 export default function Home() {
   return (
-    <main className="">
-
+    <main className=" w-full h-full">
+     <Navbar isHome={false}/>      
     </main>
   )
 }

939e205c4d1a58524818156fbf0e3c547f6a1a7b
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2023-09-08 18:02:45 -0400

    finished the navbar, also responsive

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -4,7 +4,7 @@ export default function Navbar(props?){
 	const Title_ = () => {
 		if(!props.isHome){
 			return (
-				<h1> Canlis</h1>	
+				<a href="/" className="font-crimson text-xl tracking-widest lg:text-2xl">CANLIS</a>	
 			)
 		}
 
@@ -13,46 +13,55 @@ export default function Navbar(props?){
 	const Toggle = () => {
 		setToggle(!isToggle)
 	}	
+	const AA = (props) => {
+		return (
+			<li className="font-roboto tracking-wide my-6 mr-2 text-sm lg:text-base lg:m-1 lg:font-medium">
+				<a href={props.href}>{props.text}</a>
+			</li>
+		)
+	}
 	const SideBar = () => {
 		if(isToggle){
 			return (
 				<ul className="absolute h-full w-full bg-black text-primary flex flex-col justify-center items-end top-0 right-0">
-					<li className="absolute top-0 right-0 m-2">
-						<button onClick={Toggle}>Close</button>	
+					<li className="absolute top-0 right-0 m-2 ">
+						<button onClick={Toggle} className="underline-offset-2 underline">Close</button>	
 					</li>
-					<li>
-						<a href="/reservations">RESERVATIONS</a>
-					</li>
-					<li>
-						<a href="/giftcards">GIFT CARDS</a>
-					</li>
+					<AA href="/reservations"text="RESERVATIONS"/>
+					<AA href="/giftcards" text="GIFT CARDS"/>
+
 					<li>
 						<p>-</p>
 					</li>
-					<li>
-						<a href="/menu">MENU</a>
-					</li>
-					<li>
-						<a href="/ourstory">OUR STORY</a>
-					</li>
-					<li>
-						<a href="/drinks">WINE & SPIRITS</a>
-					</li>
-					<li>
-						<a href="/events">EVENTS</a>
-					</li>
-					<li>
-						<a href="/careers">CAREERS</a>
-					</li>
+
+						<AA href="/menu" text="MENU"/>
+						<AA href="/ourstory"text="OUR STORY"/>
+						<AA href="/drinks" text="WINE & SPIRITS"/>
+						<AA href="/events" text="EVENTS"/>
+						<AA href="/careers" text="CAREERS"/>
 				</ul>
 			)
 		}
 	}
 	return(
-		<nav className="text-black flex justify-between p-2 w-full">
+		<nav className="text-black flex justify-between p-4 w-full lg:p-16">
 			<Title_ />
-			<button onClick={Toggle} className=" underline underline-offset-1 text-sm">Menu</button>	
+			<button onClick={Toggle} className=" underline underline-offset-2 text-sm lg:hidden">Menu</button>	
 			<SideBar />
+				<ul className=" w-fit h-fit flex items-end flex-col invisible absolute lg:visible lg:static">
+					<AA href="/reservations"text="RESERVATIONS"/>
+					<AA href="/giftcards" text="GIFT CARDS"/>
+
+					<li className="mr-1">
+						<p>-</p>
+					</li>
+
+					<AA href="/menu" text="MENU"/>
+					<AA href="/ourstory"text="OUR STORY"/>
+					<AA href="/drinks" text="WINE & SPIRITS"/>
+					<AA href="/events" text="EVENTS"/>
+					<AA href="/careers" text="CAREERS"/>
+				</ul>
 		</nav>
 	)
 }

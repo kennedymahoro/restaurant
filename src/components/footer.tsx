e5d98626c74c19375f0d330ce0f9f7f45ecb3a7e
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2023-09-08 18:04:16 -0400

    have the backbone done, no styling though

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,13 @@
 export default function Footer(){
+	const year = new Date().getFullYear();
 	return(
 		<footer>
-			<p>Hello, Footer</p>
-		</footer>
+			<section>
+				<p>Get eary access and a peek behind the scenes.</p>
+				<a>Sign up for the Canlis newsletter.</a>
+			</section>
+			<p>(200) 200-3000</p>
+			<p>{`© 1950 - ${year} Canlis`}</p>
+		</footer>	
 	)
 }

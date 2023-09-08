ef2355c28a415b1c68c0eb5457ba1b30a22e3840
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2023-09-08 18:00:41 -0400

    added google fonts, crimson-roboto-playfair.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,30 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Crimson_Text, Playfair_Display, Roboto } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const crimson_text = Crimson_Text({
+  subsets: ['latin'],
+  variable: '--font-crimson',
+  display: 'swap',
+  weight: '400'
+})
 
+const playfiar_display = Playfair_Display({
+  subsets: ['latin'],
+  variable: '--font-playfiar',
+  display: 'swap',
+  weight: '500'
+})
+const roboto= Roboto({
+  subsets: ['latin'],
+  variable: '--font-roboto',
+  display: 'swap',
+  weight: '400'
+})
 export const metadata: Metadata = {
   title: 'Canlis - Home',
   description: "Seattle's landmark fine-dining destination for 70 years and recent home to multi-disciplinary pandemic pivots designed to serve our city.",
-  content: '"width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport"'
+  //content: '"width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport"'
 }
 
 export default function RootLayout({
@@ -16,7 +33,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className='w-full h-full'>
+    <html lang="en" className={`w-full h-full ${crimson_text.variable} ${playfiar_display.variable} ${roboto.variable}`}>
       <body >{children}</body>
     </html>
   )

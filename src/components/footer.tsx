531c6913f359acd34a2e0e9b2219a224bd970f70
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2023-09-09 20:08:37 -0400

    finished styling the footer and added the link to newsletter:

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,13 +1,10 @@
 export default function Footer(){
 	const year = new Date().getFullYear();
 	return(
-		<footer>
-			<section>
-				<p>Get eary access and a peek behind the scenes.</p>
-				<a>Sign up for the Canlis newsletter.</a>
-			</section>
-			<p>(200) 200-3000</p>
-			<p>{`© 1950 - ${year} Canlis`}</p>
+		<footer className="text-xs font-crimson mx-4 my-7">
+			<p className="mb-2">Get eary access and a peek behind the scenes. <a className="underline underline-offset-1" href="/newsletter"> Sign up for the Canlis newsletter.</a> </p>
+			<p className="font-semibold mb-2">(200) 200-3000</p>
+			<p className="pb-8">{`© 1950 - ${year} Canlis`}</p>
 		</footer>	
 	)
 }

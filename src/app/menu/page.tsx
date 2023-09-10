05b3e2b58a5c40fa1f33f2543a252efb6360e4f6
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2023-09-09 20:07:55 -0400

    finished mobile version

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,23 +1,74 @@
-
-import Navbar from "@/components/nav"
 import Image from "next/image"
+import Navbar from "@/components/nav"
+import Footer from "@/components/footer"
 
 import plate_duck from '/public/plate_duck.jpg'
 import plate_purple from '/public/plate_purple.jpg'
+import plate_beef from '/public/plate_beef.jpg'
+import plate_fish from '/public/plate_fish.jpg'
 
 export default function Menu(){
-  return (
-    <div className="px-4">
-      <Navbar />
-      <section className=" mt-48">
-        <h1 className="text-5xl font-thinman tracking-wider my-5">Menu</h1>
-        <p className="font-crimson text-sm">We're serving a multi-course dining experience where you choose three of the courses (and we like to surprise you with several others).</p> 
-        <p className="font-crimson text-sm my-4">$175</p>
+
+  const Menu_Section_items = (props) => {
+    return(
+      <div className="my-7">
+        <p className="font-roboto text-sm mb-1">{props.name}</p>
+        <p className="text-xs">{props.items}</p>
+      </div>
+    )
+  }
+
+  const Menu_Section = (props) => {
+    return (
+      <section className="mx-5 my-16">
+        <h6 className="italic font-crimson my-4">{props.title}</h6>
+        <Menu_Section_items name={props.name1} items={props.item1} />
+        <Menu_Section_items name={props.name2} items={props.item2} />
+        <Menu_Section_items name={props.name3} items={props.item3} />
       </section>
-      <div>
-        <Image src={plate_duck} alt="image of food on a plate" />
+    )
+  }
+
+  return (
+    <div className="">
+      <Navbar />
+      <section className=" mt-48 mb-60 mx-5">
+        <h1 className="text-5xl font-thinman tracking-wider mt-5">Menu</h1>
+        <p className="font-crimson text-sm my-6">We're serving a multi-course dining experience where you choose three of the courses (and we like to surprise you with several others).</p> 
+        <p className="font-crimson text-sm mb-4">$175</p>
+      </section>
+      <div className="w-screen h-auto">
+        <Image src={plate_duck} alt="image of food on a plate"  />
         <Image src={plate_purple} alt="image of food on a plate" />
       </div>
+      <hr className="divide-solid divide-y-4 w-9/12  m-9 divide-black" />
+
+      <Menu_Section title="One" name1="SABLEFISH" item1="hazelnut, corn, kombu"
+                                name2="ARTICHOKE" item2="wildflower, spinach walnut"
+                                name3="DUCK" item3="summer squash, blueberry, dashi"/>
+
+      <Menu_Section title="Two" name1="HALIBUT" item1="snap pea, geoduck, nori"
+                                name2="EGGPLANT ZURITA" item2="Tsuyahime rice, cherry tomato, cucumber"
+                                name3="COULETTE" item3="Walla Walla onion, oyster, chive"/>
+
+      <Menu_Section title="Three" name1="SMOKED CACAO" item1="nougatine, milk ice cream"
+                                name2="STRAWBERRY" item2="rhubarb, white chololate, kasu"
+                                name3="DRIED PERSIMMON" item3="devonshire cream, vermouth, yuzu"/>
+
+      <div className="text-xs mx-4 my-10 font-crimson">
+        <p>A 20% service charge will be added to your final bill and is retained by Canlis. No additional gratuity is expected.
+        </p>
+
+        <p className="mt-2">
+          * Eating raw or undercooked ingredients can increase your risk of foodborne illness. It may also increase your chances of having a great time.
+        </p>
+      </div>
+
+      <div className="w-screen h-auto">
+        <Image src={plate_beef} alt="a plate of food, beef" />
+        <Image src={plate_fish} alt="a plate of food, fish" />
+      </div>
+      <Footer />
     </div>
   )
 }

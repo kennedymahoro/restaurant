c28b5b7a6ba16cdc4268ef711f89150fff952901
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2023-09-14 14:26:11 -0400

    removed mx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer(){
 	const year = new Date().getFullYear();
 	return(
-		<footer className="text-xs font-crimson mx-4 my-7">
+		<footer className="text-xs font-crimson my-7">
 			<p className="mb-2">Get eary access and a peek behind the scenes. <a className="underline underline-offset-1" href="/newsletter"> Sign up for the Canlis newsletter.</a> </p>
 			<p className="font-semibold mb-2">(200) 200-3000</p>
 			<p className="pb-8">{`© 1950 - ${year} Canlis`}</p>

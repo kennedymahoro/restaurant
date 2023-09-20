b4a3a1ebccada0cdab4d2c2d36e19d01d24cee7e
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2023-09-19 23:07:16 -0400

    imported Navbar and Footer

--- src/app/ourstory/page.tsx
+++ src/app/ourstory/page.tsx
@@ -1,7 +1,11 @@
+import Navbar from "@/components/nav"
+import Footer from "@/components/footer"
 export default function Ourstory(){
   return(
     <div>
+      <Navbar />
       <p>Hello</p>
+      <Footer />
     </div>
   )
 }

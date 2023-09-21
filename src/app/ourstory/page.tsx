72e7a7cde4036515dad3aff0bbf3dd2f36a74c6a
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2023-09-20 23:07:22 -0400

    continuing ourstory

--- src/app/ourstory/page.tsx
+++ src/app/ourstory/page.tsx
@@ -5,6 +5,22 @@ export default function Ourstory(){
     <div>
       <Navbar />
       <p>Hello</p>
+      <div>
+        <p>FIRST GENERATION</p>
+        <p>1950 - 1977</p>
+      </div>
+      <div>
+        <h6>WORDS BY MARK CANLIS</h6>
+        <p>
+          Family lore has it that as a very young man, Nicholas Kanlis swam five miles from the island of Lesvos, Greece to the shores of Turkey. Then he hitchhiked his way to the Mena House Hotel in Cairo where he met Teddy Roosevelt and signed on as a cook for the Smithsonian–Roosevelt African Expedition. For more than a year they traveled the rural Africa together, collecting 1100 specimens that would find their way back to the Smithsonian Institute. Eventually, Nick would reach American shores, clearing immigration with an Egyptian passport and settling down in Stockton, California.
+        </p>
+        <p>
+          Along the way he met our great grandmother, an immigrant herself--a Lebanese beauty--and the most talented cook our dad claims to have ever met. Together, they opened the first-generation Canlis restaurant in 1910. It was a mom and pop shop from the old world. A family cafe they named The Food Palace and Fish Grotto. It was heart and sweat and hope. The untamed west and the streets of Stockton, two honest, immigrant parents and “The Palace” raised our grandfather. His name was Peter Canlis.
+        </p>
+        <p>
+          In 1939, Peter left the family and its restaurant and struck out on his own. Maybe he was keeping in his father’s tradition. A few who knew him said he had something to prove—a vision too grand for his family’s small cafe to bear. With the untested confidence of youth and a first-generation immigrant chip on his shoulder, he left to make a name for himself in Hawaii. It was then a place of passenger ships and Pan-American Airways, royal palaces and rows of palms on underdeveloped beaches. This paradise was full of promise. And war.
+        </p>
+      </div>
       <Footer />
     </div>
   )

cb0ed848a101e54bbda6baeff48ad7f62db0208e
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2023-09-23 21:50:05 -0400

    added footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 //import Image from 'next/image'
 import Navbar from "@/components/nav"
-//import Footer from "@/components/footer"
+import Footer from "@/components/footer"
 
 export default function Home() {
   return (
     <main className=" w-screen h-screen bg-primary">
      <Navbar isHome={false}/>      
+      <Footer />
     </main>
   )
 }

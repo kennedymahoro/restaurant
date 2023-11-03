03e8a8310c282da0378d5cc82d947594b666f79a
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2023-11-02 23:11:28 -0400

    third gen, mx-10

--- src/app/story/page.tsx
+++ src/app/story/page.tsx
@@ -153,7 +153,8 @@ export default function Ourstory(){
           <h6 className="text-xs font-roboto">THIRD GENERATION</h6>
           <h6 className="text-xs font-roboto">2007 - PRESENT</h6>
         </div>
-        <div className="lg:flex">
+
+        <div className="lg:flex lg:mx-10">
           <div className="lg:flex lg:flex-col lg:w-8/12">
             <div className="w-fit h-auto transform -rotate-6 my-9">
               <Image src={markbrain} alt="Picture of the peter on the phone" />

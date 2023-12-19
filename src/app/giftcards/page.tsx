0a9382801bcaf4ec30b9ec44593e0199210d18c2
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2023-12-19 00:49:54 -0500

    h1 title changed font to thinman, 4xl and <main> to mx-4

--- src/app/giftcards/page.tsx
+++ src/app/giftcards/page.tsx
@@ -8,8 +8,8 @@ export default function Page() {
         <Navbar />
       </div>
 
-      <main>
-        <h1>Give Canlis.</h1>
+      <main className="mx-4">
+        <h1 className="font-thinman text-4xl">Give Canlis.</h1>
         <p>Gift cards are available in any amount; we recommend $500 for a dinner for two. We process orders on the following business day. Please allow 5-7 days for shipping. Gift cards are non-refundable.</p>
 
         {/* Form goes here */}
